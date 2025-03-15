99875960ae78970f5b6ffa9551d1f8807fa3314d
Ethan Peters <ethanjackpeters@hotmail.com>
2025-03-15 20:37:24 +0000

    Integrate Stripe payment processing by adding necessary JavaScript and CSS for payment elements. Update checkout templates to include Stripe public key and client secret for secure transactions. Enhance requirements with additional dependencies for Stripe integration.

--- checkout/views.py
+++ checkout/views.py
@@ -8,10 +8,12 @@ def checkout(request):
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('products'))
-    
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51R303PGscFKbkzBlJHUxKQzKlHCKLa7ZjYSa6UzsNuyMqfUDwGm7NVqqpELYppItzCxQNN830L72c39sIZ1OlKFK00gl4S3a2I', # noqa
+        'client_secret': 'test client secret',
     }
+
     return render(request, template, context)
