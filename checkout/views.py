85351aef1d448018f4609b673b2d5fd022c4488c
Ethan Peters <ethanjackpeters@hotmail.com>
2025-03-16 16:12:52 +0000

    Enhance checkout process by implementing loading overlay and spinner during payment submission. Update views to handle Stripe payment intents and ensure client secret is conditionally rendered. Add CSS for loading elements and update JavaScript to toggle visibility during form submission and error handling.

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
+    intent = None
+
     if request.method == 'POST':
         bag = request.session.get('bag', {})
         form_data = {
@@ -62,6 +64,15 @@ def checkout(request):
             messages.error(request, 'There was an error with your form. \
                 Please double check your information.')
 
+            current_bag = bag_contents(request)
+            total = current_bag['grand_total']
+            stripe_total = round(total * 100)
+            stripe.api_key = stripe_secret_key
+            intent = stripe.PaymentIntent.create(
+                amount=stripe_total,
+                currency=settings.STRIPE_CURRENCY,
+            )
+
     else:
         bag = request.session.get('bag', {})
         if not bag:
@@ -88,7 +99,7 @@ def checkout(request):
     context = {
         'order_form': order_form,
         'stripe_public_key': stripe_public_key,
-        'client_secret': intent.client_secret,
+        'client_secret': intent.client_secret if intent else '',
     }
 
     return render(request, template, context)
