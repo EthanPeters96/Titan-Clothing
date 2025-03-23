34b9e2d1dab867cd464340da616fdb2e5186a23a
Ethan Peters <ethanjackpeters@hotmail.com>
2025-03-23 22:47:39 +0000

    Updated requirements.txt with additional dependencies for AWS integration and modified settings.py to support database configuration via environment variable.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn titan-clothing.wsgi:application
--- titan_clothing/settings.py
+++ titan_clothing/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os # noqa
+import dj_database_url # noqa
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -34,6 +35,7 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
     'titan-clothing.herokuapp.com',
     'localhost',
+    'titan-clothing-c5f25b636dd8.herokuapp.com',
 ]
 
 
@@ -127,15 +129,17 @@ LOGIN_REDIRECT_URL = '/'
 WSGI_APPLICATION = 'titan_clothing.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else: 
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
