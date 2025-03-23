6ff8bbe152a2b2754e5cbacbb3de2d723119017f
Ethan Peters <ethanjackpeters@hotmail.com>
2025-03-23 23:40:19 +0000

    Enhance settings.py for AWS integration: added AWS storage configurations for static and media files, and updated URLs to use S3 custom domain.

--- titan_clothing/settings.py
+++ titan_clothing/settings.py
@@ -57,9 +57,11 @@ INSTALLED_APPS = [
     'bag',
     'checkout',
     'profiles',
+    
 
     # Other apps
     'crispy_forms',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -183,6 +185,26 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+if 'USE_AWS' in os.environ:
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_STORAGE_BUCKET_NAME = os.environ.get('titan-clothing')
+    AWS_S3_REGION_NAME = os.environ.get('eu-north-1')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+# Static files
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+STATICFILES_LOCATION = 'static'
+
+# Media files
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+MEDIAFILES_LOCATION = 'media'
+
+# Override static and media URLs in production
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
