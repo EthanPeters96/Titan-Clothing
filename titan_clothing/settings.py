4ad6e9026683a3519f4b103ab2c6d6ac12be2e73
Ethan Peters <ethanjackpeters@hotmail.com>
2025-03-25 21:27:59 +0000

    Update image source in base.html to use MEDIA_URL for better asset management and refactor email settings in settings.py for clarity and consistency. Ensured EMAIL_HOST and EMAIL_PORT are defined correctly in production environment.

--- titan_clothing/settings.py
+++ titan_clothing/settings.py
@@ -222,11 +222,12 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET')
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'titanclothing3@gmail.com'
+
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_PORT = 587
     EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PASS = os.environ.get('EMAIL_HOST_PASSWORD')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
