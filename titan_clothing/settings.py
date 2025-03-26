032b5e568851faf6e05549836577f29357244887
Ethan Peters <ethanjackpeters@hotmail.com>
2025-03-26 22:25:03 +0000

    Update default email sender in settings.py for consistency across environments
    - Changed DEFAULT_FROM_EMAIL in development settings from a Gmail address to a noreply address for better branding.
    - Updated production settings to use a more appropriate environment variable for the default email sender.

--- titan_clothing/settings.py
+++ titan_clothing/settings.py
@@ -232,7 +232,7 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 # Email
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'titanclothing3@gmail.com'
+    DEFAULT_FROM_EMAIL = 'noreply@titan-clothing-app.com'
 
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
@@ -241,4 +241,4 @@ else:
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-    DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
+    DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_USER')
