99fddf66342d2694145a060a99fd51877203310f
Ethan Peters <ethanjackpeters@hotmail.com>
2025-03-27 22:36:32 +0000

    Refactor email handling in contact form to streamline sending process
    - Simplified email sending logic in the contact view by removing conditional checks for development and production environments.
    - Updated settings to use Gmail SMTP for email delivery, ensuring consistent email handling across environments.
    - Enhanced error handling in the contact view for better user feedback on email sending issues.

--- titan_clothing/settings.py
+++ titan_clothing/settings.py
@@ -239,9 +239,9 @@ if 'DEVELOPMENT' in os.environ:
     DEFAULT_FROM_EMAIL = 'titanclothing3@gmail.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = 'smtp.sendgrid.net'
-    EMAIL_PORT = 587
     EMAIL_USE_TLS = True
-    EMAIL_HOST_USER = 'apikey'  # This is literally the string 'apikey'
-    EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_API_KEY')
-    DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
